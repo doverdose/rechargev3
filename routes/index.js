032cf2b4caf948ea8b46ef9619dce1cb3bad2a3b
Royce <royce.cheng@gmail.com>
2013-09-26 23:52:01 +0000

    Intermediate state with create checkin working

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,50 @@
+var mongoose = require('mongoose');
+
+
 exports.index = function(req,res){
     res.render('index.html');
-};
+}
 
 exports.dashboard = function(req, res) {
     res.render('dashboard.html');
-};
+}
 
-exports.form = function(req, res) {
+exports.checkin_update = function(req, res) {
+    Checkin.findById(req.body.checkin.id, function(c) {
+	c.data = req.body.checkin.data;
+	c.save(function() {
+	    switch (req.params.format) {
+		case 'json':
+		  res.send(c.__doc);
+		  break;
+		default:
+		  res.redirect('/checkin');
+	    }
+	});
+    });
+}
+
+exports.checkin_create = function(req, res) {
+    var Checkin = mongoose.model('Checkin');
+    console.log(req);
+    var c = new Checkin(req.body.checkin);
+    c.save(function() {
+	switch (req.params.format) {
+	case 'json':
+	    res.send(c.__doc);
+	    break;
+	default:
+	    res.redirect('/checkin');
+	}
+    });
+    
+}
+
+exports.checkin_new = function (req, res) {
+    res.render('checkin_new.ejs');
+}
+
+exports.checkin = function(req, res) {
     res.render('form.html');
-};
+}
 
