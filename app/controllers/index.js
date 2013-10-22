ae9f77ef107e402304720a6d5d589a4b52a08a09
Cristian Colceriu <cristian.colceriu@barandisolutions.ro>
2013-10-22 10:43:42 +0300

    Add missing Procfile.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,11 +1,11 @@
-var mongoose = require('mongoose')
-  , util = require('util');
+var mongoose = require('mongoose'),
+	util = require('util');
 
 // Error handling
 function NotFound(msg) {
-    this.name = "Not Found";
-    Error.call(this, msg);
-    Error.captureStackTrace(this, arguments.callee);
+	this.name = "Not Found";
+	Error.call(this, msg);
+	Error.captureStackTrace(this, arguments.callee);
 }
 
 util.inherits(NotFound, Error);
@@ -13,7 +13,7 @@ util.inherits(NotFound, Error);
 exports.NotFound = NotFound;
 
 exports.index = function(req,res){
-    res.render('../views/static/index.ejs');
+	res.render('../views/static/index.ejs');
 }
 
 exports.dashboard = function(req, res) {
@@ -21,57 +21,57 @@ exports.dashboard = function(req, res) {
 }
 
 exports.checkin_update = function(req, res) {
-    Checkin.findOne({_id: req.params.id}, function(err, c) {
-	if (!c) return next(new NotFound('Checkin not found'));
-	c.data = req.body.checkin.data;
-	c.save(function() {
-	    switch (req.params.format) {
-		case 'json':
-		  res.send(c.__doc);
-		  break;
-		default:
-		  res.redirect('/checkin');
-	    }
-	});
+	Checkin.findOne({_id: req.params.id}, function(err, c) {
+		if (!c) return next(new NotFound('Checkin not found'));
+		c.data = req.body.checkin.data;
+		c.save(function() {
+			switch (req.params.format) {
+			case 'json':
+			res.send(c.__doc);
+			break;
+			default:
+			res.redirect('/checkin');
+			}
+		});
     });
 }
 
 exports.checkin_edit = function(req, res, next) {
     Checkin.findOne({_id: req.params.id}, function(err, c) {
-	if (!c) return next(new NotFound('Checkin not found'));
-	switch (req.params.format) {
-	case 'json':
-	    res.send(c.__doc);
-	    break;
-	default:
-	    res.render('checkin_edit.ejs', {c: c});
-	}
+		if (!c) return next(new NotFound('Checkin not found'));
+		switch (req.params.format) {
+		case 'json':
+			res.send(c.__doc);
+			break;
+		default:
+			res.render('checkin_edit.ejs', {c: c});
+		}
     });
 }
 
 exports.checkin_create = function(req, res) {
-    var c = new Checkin(req.body.checkin);
-    c.save(function() {
-	switch (req.params.format) {
-	case 'json':
-	    res.send(c.__doc);
-	    break;
-	default:
-	    res.redirect('/checkin');
-	}
+	var c = new Checkin(req.body.checkin);
+	c.save(function() {
+		switch (req.params.format) {
+			case 'json':
+				res.send(c.__doc);
+				break;
+			default:
+				res.redirect('/checkin');
+		}
     });
     
 }
 
 exports.checkin_new = function (req, res) {
-    res.render('checkin_new.ejs', {c: {}});
+	res.render('checkin_new.ejs', {c: {}});
 }
 
 exports.checkin = function(req, res) {
-    var Checkin = mongoose.model('Checkin');
-    Checkin.find({}, function(err, checkins){
-	if (err) throw err;
-	res.render('checkin.ejs', {c: checkins});
-    });
+	var Checkin = mongoose.model('Checkin');
+	Checkin.find({}, function(err, checkins){
+		if (err) throw err;
+		res.render('checkin.ejs', {c: checkins});
+	});
 }
 
