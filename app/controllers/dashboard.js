0cca5081d20dea53ee7f559fe48ca41b13d8a136
Cristian Colceriu <cristian.colceriu@barandisolutions.ro>
2013-10-24 15:16:45 +0300

    Get and render Check-ins for the current week.

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose'),
 	util = require('util'),
-	User = mongoose.model('User');
+	User = mongoose.model('User'),
+	Checkin = mongoose.model('Checkin'),
+	weekDays = 7 * 24 * 60 * 60 * 1000;
 
 var loggedInThisWeek = function(callback) {
-
-	var weekDays = 7 * 24 * 60 * 60 * 1000;
 
 	User.find({
 		last_login: { $gte: new Date().getTime() - weekDays }
@@ -16,12 +16,31 @@ var loggedInThisWeek = function(callback) {
 
 };
 
+var checkinsThisWeek = function(callback) {
+
+	Checkin.find({
+		timestamp: { $gte: new Date().getTime() - weekDays }
+	}, function(err, allUsers) {
+		if (err) callback(err);
+
+		callback(null, allUsers.length);
+	});
+
+};
+
 exports.dashboard = function(req, res) {
 
-	loggedInThisWeek(function(err, usersThisWeek) {
-		res.render('dashboard/dashboard.ejs', {
-			usersThisWeek: usersThisWeek
+	loggedInThisWeek(function(err, users) {
+
+		checkinsThisWeek(function(err, checkins) {
+
+			res.render('dashboard/dashboard.ejs', {
+				usersThisWeek: users,
+				checkinsThisWeek: checkins
+			});
+
 		});
+
 	});
 
 };
